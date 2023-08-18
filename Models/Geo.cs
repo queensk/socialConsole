cc47170f0e923bd61a490d38dcb9592974699b25
queens kisivuli <queenskisivuli@gmail.com>
2023-08-18 22:01:37 +0300

    user geo

---
+++ Models/Geo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace socialConsole.Models
+{
+
+    public class Geo
+    {
+        public string Lat { get; set; }
+        public string Lng { get; set; }
+    }
+}
