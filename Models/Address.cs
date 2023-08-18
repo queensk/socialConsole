184a215b5d07bea849e53317eaf49b26031026ec
queens kisivuli <queenskisivuli@gmail.com>
2023-08-18 21:55:42 +0300

    user address model

---
+++ Models/Address.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace socialConsole.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public Geo Geo { get; set; }
+    }
+}
